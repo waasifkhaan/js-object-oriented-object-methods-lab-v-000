c3156d6c19f5717c155fec9f9cf2434bcf1564b8
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 00:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function BoardMember(name, homeState, training) {
      return "I like to help people.";
   this.releasePressStatement = function () {
     return "You will see great things from Scuber.";
-  this.sayHi = function () {
-    return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in ${this.training}.`;
-
-  }
+  
 
 }
