439458cced6e014e1bc9db01b443edfa05b0ebd3
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 00:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function BoardMember(name, homeState, training) {
     return "You will see great things from Scuber.";
   }
   this.sayHi = function () {
-    return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in ${this.training}.`;
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 }
 
 
