0174d5b443a7a21ebb0d3b779d2b6f12bf3cd556
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 00:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function BoardMember(name, homestate, training) {
-  this.training = training
-  this.homestate = homestate
-  this.name = name
+  this.training = training;
+  this.homestate = homestate;
+  this.name = name;
 
   this.veto = function () {
      console.log("No, I must disagree");
