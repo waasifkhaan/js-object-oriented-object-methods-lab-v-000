fa91abc5e9a3bec069497bd26d497610d8dc4e5d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 00:54:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.name = name;
 
+  this.veto = function () {
+     return "No, I must disagree";
+   }
+  this.approve = function () {
+     return "You can do that!";
+   }
+  this.doCharity = function () {
+     return "I like to help people.";
+   }
+  this.releasePressStatement = function () {
+    return "You will see great things from Scuber.";
+  }
+  this.sayHi = function () {
+    return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in ${this.training}.`;
+}
 
-  
 
 }
